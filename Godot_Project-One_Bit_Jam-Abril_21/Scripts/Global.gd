f68d0417ffa8ed8e5d2d5883f0d1fb8b1791ce9b
Rapiere Gridoro <rapieregridoro.gmail.com>
2021-04-03 20:34:16 -0300

    contato dash

--- Godot_Project-One_Bit_Jam-Abril_21/Scripts/Global.gd
+++ Godot_Project-One_Bit_Jam-Abril_21/Scripts/Global.gd
@@ -19,8 +19,8 @@ func _process(delta):
 
 func wished_direction_inputs():
 	#pega os inputs do jogador
-	wished_direction.x = (int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")))
-	wished_direction.y = (int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up")))
+	wished_direction.x = (int(Input.is_action_pressed("ptb_right")) - int(Input.is_action_pressed("ptb_left")))
+	wished_direction.y = (int(Input.is_action_pressed("ptb_down")) - int(Input.is_action_pressed("ptb_up")))
 	interacting = Input.is_action_just_pressed("ui_select")
 	if interacting:
 		emit_signal("interacting_signal")
